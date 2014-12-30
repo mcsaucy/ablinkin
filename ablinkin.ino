add2d27cfeea2553ce661d0405250faf3f626a9b
Josh McSavaney <mcsaucy@csh.rit.edu>
2014-12-30 12:49:33 -0500

    Minor restructuring with some added comments

--- ablinkin.ino
+++ ablinkin.ino
@@ -19,9 +19,36 @@
 Board * segment;
 byte strlength;
 char * string;
+int address;
 
-void setup()
-{
+// Write out characters to a given board segment, curboard.
+// Curboard is actually a Node in a linked list.
+void writeOut( Board * curboard ) {
+    // While we still have stuff in our linked list
+    while ( curboard != NULL ) {
+        Wire.beginTransmission( curboard->address );
+        Wire.write( get_char_section( string[i], j ) );
+        Wire.endTransmission();
+        Wire.requestFrom( curboard->address, (byte) 1 );
+        if ( Wire.available() ) {
+            Wire.read();
+        }
+        curboard = curboard->next;
+    }
+}
+
+// Read in our address from a series of dip switches
+int readAddress() {
+    int addr = 0;
+    for( uint8_t i = 2; i < 9; i++ ) {
+        pinMode(i, INPUT);
+        addr |= digitalRead(i) << (i - 2);
+    }
+
+    return addr;
+}
+
+void setup() {
     string = "/\\";
     strlength = strlen(string);
     segment = NULL;
@@ -29,50 +56,27 @@ void setup()
     first = 0;
     last = 0;
 
-    for( uint8_t i = 2; i < 9; i++ )
-    {
-        pinMode(i, INPUT);
-        address |= digitalRead(i) << (i - 2);
-    }
+    address = addr;
 
-    if ( address == 0 )
-    {
+    if ( address == 0 ) {
         Serial.begin(9600);
         Wire.begin();
 
-        do
-        {
+        do {
             delay(100); // allow slaves to come up/wait 100ms and try again
-        }
-        while ( scanAndPopulate( segment ) == 0 ); // keep trying til it works
-        
-    }
-    else
-    {
+        } while ( scanAndPopulate(segment) == 0 ); // it'll work eventually
+ 
+    } else {
         Wire.begin(address);
         initSlave();
     }
 }
 
-void loop()
-{
-    if ( address == 0 )
-    {   
-        for ( int i = 0; i < strlength; i++ )
-        {
-            for ( int j = 0; j < 4; j++ )
-            {
-                Board * curboard = segment;
-                while ( curboard != NULL )
-                {
-                    Wire.beginTransmission( curboard->address );
-                    Wire.write( get_char_section( string[i], j ) );
-                    Wire.endTransmission();
-                    Wire.requestFrom( curboard->address, (byte) 1 );
-                    if ( Wire.available() )
-                        Wire.read();
-                    curboard = curboard->next;
-                }
+void loop() {
+    if ( address == 0 ) {   
+        for ( int i = 0; i < strlength; i++ ) {
+            for ( int j = 0; j < 4; j++ ) {
+                writeOut(segment);
             }
             delay(100);
         }
